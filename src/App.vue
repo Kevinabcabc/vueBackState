f0177fa3f858b3144b0ac43d4850914588323f85
Kevinabcabc <1356075038@qq.com>
2020-03-11 20:39:57 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <div>app</div>
+  </div>
+</template>
+
+<style lang="scss">
+#app{
+  height: 100%;
+  width: 100%;
+  background: red;
+  border: 1px solid black;
+}
+</style>
