49e959ac9be5098c88b67a7b8ebc80da2f2d60b2
Kevinabcabc <1356075038@qq.com>
2020-03-11 22:44:40 +0800

    ui

--- src/views/A.vue
+++ src/views/A.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="a">
     <h1>a model </h1>
+    <Button type="primary">
+      elemnet ui button
+    </Button>
+    <Select>
+    </Select>
   </div>
 </template>
+
+<script>
+import Vue from 'vue'
+import { Button, Select } from 'element-ui'
+Vue.use(Button).use(Select)
+export default {
+  name: 'A',
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss">
+#app{
+  height: 100%;
+  width: 100%;
+  background: red;
+  border: 1px solid black;
+}
+</style>
